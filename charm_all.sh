1003f04346749f9c14d3e4640e597bf9a75d474d
jshanna100 <jeff.hanna@gmail.com>
2023-03-27 12:42:06 +0200

    b

--- charm_all.sh
+++ charm_all.sh
@@ -1,6 +1,7 @@
  #!/usr/bin/bash
 
 subjs=("001" "003" "004" "005" "006" "008" "009" "012" "013" "016" "017" "018" "020" "022" "024" "025" "027" "028" "029" "030" "032" "033" "034" "035" "062" "064" "066" "072" "074" "079" "080" "081" "086" "089" "091" "095" "097" "098" "105" "106" "994" "995" "996" "997" "998" "999")
+subjs=("995" "996" "997" "998" "999")
 orig_dir="/media/Linux5_Data03/hannaj/simnibs/4/"
 #orig_dir="/home/jev/simnibs/4/"
 
@@ -8,6 +9,6 @@ for subj in "${subjs[@]}"
 do
     echo "$subj"
     cd "$orig_dir""$subj"
-    charm $subj T1w.nii T2w.nii --forcerun --forceqform
+    charm $subj T1w.nii.gz T2w.nii.gz --forcerun --forceqform
 done
 cd "$orig_dir"
